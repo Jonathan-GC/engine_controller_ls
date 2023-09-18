8ad2f8855f1693ab2edbafa850cc851f05fbc5c0
Jonathan-GC <jonathangc.awt@gmail.com>
2023-09-17 20:22:12 -0500

    Obtencion de Caracteristica del rostro en video

--- PRUEBA ROSTRO.py
+++ PRUEBA ROSTRO.py
@@ -2,7 +2,7 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 def draw_landmarks_on_image(rgb_image, detection_result):
   face_landmarks_list = detection_result.face_landmarks
   
@@ -43,8 +43,8 @@ def draw_landmarks_on_image(rgb_image, detection_result):
   return annotated_image
 
 def plot_face_blendshapes_bar_graph(face_blendshapes):
-  print("Gola")
-  '''# Extract the face blendshapes category names and scores.
+  
+  # Extract the face blendshapes category names and scores.
   face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
   face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
   # The blendshapes are ordered in decreasing score value.
@@ -62,7 +62,7 @@ def plot_face_blendshapes_bar_graph(face_blendshapes):
   ax.set_xlabel('Score')
   ax.set_title("Face Blendshapes")
   plt.tight_layout()
-  plt.show()'''
+  plt.show()
 
 
 import cv2
@@ -97,86 +97,49 @@ with FaceLandmarker.create_from_options(options) as landmarker:
       
       imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
       imagen = cv2.resize(imagen, (0,0), fx=0.4, fy=0.4)
-      mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=imagen)
-
-      timeNow
-      face_landmarker_result = landmarker.detect_for_video(mp_image, timeNow)
-      timeNow += 1000//fps
-      
-      annotated_image = draw_landmarks_on_image(imagen, face_landmarker_result)
       cv2.imshow("imagen1", imagen)
-      cv2.imshow("imagen2", annotated_image)
-      #plot_face_blendshapes_bar_graph(face_landmarker_result.face_blendshapes[0])
-      if cv2.waitKey(1) == 27:
+
+      if (cv2.waitKey(1) == 27):
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=imagen)
+        timeNow
+        face_landmarker_result = landmarker.detect_for_video(mp_image, timeNow)
+        timeNow += 1000//fps
+      
+        annotated_image = draw_landmarks_on_image(imagen, face_landmarker_result)
+      
         
+        cv2.imshow("imagen2", annotated_image)
         break
+      
+      #plot_face_blendshapes_bar_graph(face_landmarker_result.face_blendshapes[0])
 
 
 
 
+cap.release()
 cv2.destroyAllWindows()
-print(face_landmarker_result)
 
 
+for element in face_landmarker_result.face_blendshapes[0]:
+  print(element.index, element.category_name, element.score)
 
+caracteristicas = face_landmarker_result.face_blendshapes[0]
 
+nombreCaracteristicas = [caracteristica.category_name for caracteristica in caracteristicas]
+calificacionCaracteristicas = [calificacion.score for calificacion in caracteristicas] 
 
+rangos = range(len(nombreCaracteristicas))
 
+fig, ax = plt.subplots(figsize=(12,12))
+bar = ax.barh(rangos, calificacionCaracteristicas, label = [str(x) for x in rangos])
+ax.set_yticks(rangos, nombreCaracteristicas)
+ax.invert_yaxis()
 
+#etiquetar cada barra
+for score, patch in zip(calificacionCaracteristicas, bar.patches):
+  plt.text(patch.get_x() +  patch.get_width(), patch.get_y(), f"{score: .4f}", va = "top")
 
-
-'''
-#img = cv2.imread("sources/image.png")
-
-#cv2.imshow("imagen", img)
-
-# STEP 1: Import the necessary modules.
-import mediapipe as mp
-BaseOptions = mp.tasks.BaseOptions
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from mediapipe.python._framework_bindings import image as image_module
-# STEP 2: Create an FaceLandmarker object.
-base_options = python.BaseOptions(model_asset_path='modelos/face_landmarker.task')
-options = vision.FaceLandmarkerOptions(running_mode = 2,
-                                       base_options=base_options,
-                                       output_face_blendshapes=True,
-                                       output_facial_transformation_matrixes=True,
-                                       num_faces=1)
-detector = vision.FaceLandmarker.create_from_options(options)
-
-
-while(1):
-  ret, imagen = cap.read()
-
-  if not ret:
-    break
-  else:
-    cv2.imshow("imagen2", imagen)
-
-    imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=imagen)
-
-    detection_result = detector.detect_for_video(mp_image,
-                                       timestamp_ms = 30)
-    #mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=imagen)
-
-    if cv2.waitKey(1) == 27:
-      break
-
-
-
-# STEP 3: Load the input image.
-#image = mp.Image.create_from_file("sources/image.png")
-#print(image)
-# STEP 4: Detect face landmarks from the input image.
-#detection_result = detector.detect(image)
-
-# STEP 5: Process the detection result. In this case, visualize it.
-#annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-#cv2.imshow("imagen2", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-
-#plot_face_blendshapes_bar_graph(detection_result.face_blendshapes[0])
-
-
-cv2.destroyAllWindows()'''
+ax.set_label("Calificacion")
+ax.set_title("Caracteristicas Rostro")
+plt.tight_layout()
+plt.show()
