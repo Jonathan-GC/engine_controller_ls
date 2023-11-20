79ad56a0ee152f9ee3b29615d139f92e7090151a
Jonathan-GC <jonathangc.awt@gmail.com>
2023-11-19 22:12:50 -0500

    Agregando un GUI

--- extractText/extractor.py
+++ extractText/extractor.py
@@ -8,6 +8,34 @@ reader = easyocr.Reader(["es"], gpu=False)
 captura = cv2.VideoCapture("sources/Dictionary/personas/2.mp4")
 
 
+#Roi Definido por dos puntos
+p1, p2 = None, None
+estado = 0
+
+def seleccionar_area_de_texto(event, x, y, flags, param):
+    global estado, p1, p2
+
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+       #Seleccionar primer punto
+        if estado == 0:
+            p1 = (x, y)
+            estado += 1
+            print("Hola")
+            
+        #selecionar segundo punto
+        elif estado == 1:
+            p2 = (x, y)
+            estado += 1
+            print("Hola2")
+    
+    if event == cv2.EVENT_RBUTTONUP:
+        
+        p1, p2 = None, None
+        estado = 0
+
+
+
+
 def analisiText(img):
     result = reader.readtext(img, paragraph=False)
     #return result
@@ -21,7 +49,9 @@ HILOS = []
 
 
     
-
+#introducir los callBack del mouse
+cv2.namedWindow('video')
+cv2.setMouseCallback("video", seleccionar_area_de_texto)
 
 
 while 1:
@@ -31,18 +61,25 @@ while 1:
     if not _:
         break
     else:
+        # si el roi esta seleccionado dibujelo
+        if estado  > 1:
+            cv2.rectangle(imagen, p1, p2, (255, 0, 0), 10)
+        
+        
+        
         fotograma += 1
         cv2.imshow("video", imagen)
 
         if fotograma % 180:
             pass
 
-        
-        #if cv2.waitKey(1) == 27:
-        #    break
         if cv2.waitKey(9) == 9:
-            #imagenes.append(fotograma)
-            HILOS.append(threading.Thread(target=analisiText, args=(imagen,)))
+            #Si hay puntos registre el fotograma
+            if p1 != None and p2 !=None:
+                #Capture el area de interes  
+                imagen_area_interes = imagen[int(p1[1]) : int(p2[1]), int(p1[0]) : int(p2[0])]
+                #Procese el texto
+                HILOS.append(threading.Thread(target=analisiText, args=(imagen_area_interes,)))
         elif cv2.waitKey(1) == 27:
             break
 
