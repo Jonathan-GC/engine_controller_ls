78d7ce3fcee9e62df49413737dccd70ec5dc8225
Jonathan-GC <jonathangc.awt@gmail.com>
2023-12-07 09:53:09 -0500

    adelantos de la GUI

--- extractText/funciones/reproductor.py
+++ extractText/funciones/reproductor.py
@@ -12,8 +12,11 @@ class Visualizador_Video:
     # y terminar los procesos he hilos de ejecución
     last_task = None
     #Roi Definido por dos puntos
-    p1, p2 = None, None
+    p1, p2 = None, None # Roi Persona
+    p3, p4 = None, None # Roi Texto
+    
     estado = 0
+    estado1 = 0
     dimensiones = (480, 640)
     imagen_general = np.zeros(dimensiones, np.uint8)
 
@@ -58,9 +61,24 @@ class Visualizador_Video:
 
                     if self.mostrar_ventana:
                         # si el roi esta seleccionado dibujelo
-                        if self.estado  > 1:
-                            cv2.rectangle(self.imagen_general, self.p1, self.p2, (255, 0, 0), 10)
-                        
+                        if self.estado  > 1 and self.estado < 3:
+                            cv2.rectangle(self.imagen_general, self.p1, self.p2, (255, 0, 0), 5)
+                            imagen_persona = self.imagen_general[int(self.p1[1]) : int(self.p2[1]), int(self.p1[0]) : int(self.p2[0])]
+                            imagen_persona = imutils.resize(imagen_persona, height=250)
+                            imagen_persona = cv2.cvtColor(imagen_persona, cv2.COLOR_BGR2RGB)
+                            im_persona = Image.fromarray(imagen_persona)
+                            img_persona = ImageTk.PhotoImage(image=im_persona)
+                            self.etiquetaRoiPersonaje.configure(image=img_persona)
+                            self.etiquetaRoiPersonaje.image = img_persona
+                        if self.estado  > 2 and self.estado < 5:
+                            cv2.rectangle(self.imagen_general, self.p3, self.p4, (0, 255, 0), 5)
+                            imagen_texto = self.imagen_general[int(self.p3[1]) : int(self.p4[1]), int(self.p3[0]) : int(self.p4[0])]
+                            imagen_texto = imutils.resize(imagen_texto, height=250)
+                            imagen_texto = cv2.cvtColor(imagen_texto, cv2.COLOR_BGR2RGB)
+                            im_texto = Image.fromarray(imagen_texto)
+                            img_texto = ImageTk.PhotoImage(image=im_texto)
+                            self.etiquetaRoiText.configure(image=img_texto)
+                            self.etiquetaRoiText.image = img_texto
                         cv2.imshow("Imagen", self.imagen_general)
 
 
@@ -90,24 +108,36 @@ class Visualizador_Video:
     #Llamada a eventos del mouse
     def seleccionar_area(self, event, x, y, flags, param):
         # Ejemplos de acciones con algunos eventos del mouse
+               
         if event == cv2.EVENT_LBUTTONDBLCLK:
-            #cv2.putText(self.imagen_general, "Seleccionar", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            #cv2.putText(self.imagen_general, "Seleccionar", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2, cv2.LINE_AA) 
+            
+            if  flags == cv2.EVENT_FLAG_ALTKEY and  self.estado1 == 0:
+                print("JOder1")
+                self.p3 = (x, y)
+                self.estado1 = 1
+            elif flags == cv2.EVENT_FLAG_ALTKEY and self.estado1 == 1:
+                print("JOder2")
+                self.p4 = (x, y)
+                self.estado1 = 2
             
             #Seleccionar primer punto
-            if self.estado == 0:
+            elif self.estado == 0:
                 self.p1 = (x, y)
                 self.estado = 1
             #selecionar segundo punto
             elif self.estado == 1:
                 self.p2 = (x, y)
                 self.estado = 2
+            
+
 
         if event == cv2.EVENT_RBUTTONUP:
             self.p1, self.p2 = None, None
             self.estado = 0
-
-        if flags == cv2.EVENT_FLAG_ALTKEY:
-            print("JODER")
+        elif flags == cv2.EVENT_FLAG_ALTKEY and event == cv2.EVENT_RBUTTONUP:
+            self.p3, self.p4 = None, None
+            
 
     def motrar_roi(self):
         
