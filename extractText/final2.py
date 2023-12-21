458877ddd9f8c27629e649bf3c0c49aa1e8b4a7a
Jonathan-GC <jonathangc.awt@gmail.com>
2023-12-21 12:44:43 -0500

    mejoras del la GUI

--- extractText/final2.py
+++ extractText/final2.py
@@ -1,8 +1,32 @@
 #librerias
+from struct import pack
 from tkinter import *
 from tkinter import ttk
+
 import tkinter
-from turtle import window_width
+from turtle import left, window_width
+from idlelib.tooltip import Hovertip
+from torch import fill #para la informacion de los botones
+from ttkwidgets import CheckboxTreeview
+
+#Funcion Global para seleccionar el objeto y ponerlo en 
+# la vista general De Video
+def item_selected(event):
+    """
+    Evento invocado cuando el contenido de una carpeta es abierto.
+    """
+    item_seleccionado = arbol.selection()
+    item = arbol.item(item_seleccionado)
+    
+    #Extraer la ruta del archivo
+    ruta = item["values"][0]
+    
+    print(ruta)
+    #global RUTA
+    #RUTA = ruta
+    #Iniciacion del video
+    #Visualizador_General.iniciar_video(ruta)
+    #iniciar_video(ruta, lblVideo)
 
 
 #Ventana principal
@@ -11,26 +35,93 @@ frame_root = Tk()
 #Configuraciones de la ventana
 frame_root.title("Sistema de Mineria TecnoBot")
 frame_root.geometry("1080x720")
-"""
-frame_root.columnconfigure(0, weight=1)
-frame_root.columnconfigure(1, weight=3)
-frame_root.columnconfigure(2, weight=1)
-frame_root.rowconfigure(0, weight=1, minsize=20)
-frame_root.rowconfigure(1, weight=1)
-frame_root.rowconfigure(2, weight=1)
-frame_root.rowconfigure(3, weight=1)
-"""
+frame_root.config(bg='white')
+
 #Crecion de 4 raices
 
-#Barra de herramientas
+#----------------------------------------------------
+#                 Barra herramientas
+#----------------------------------------------------
 frame_tools_bar = Frame(frame_root)
 frame_tools_bar.pack( fill='x', side='top')
-frame_tools_bar.config(bg="blue", height=50)
+frame_tools_bar.config(height=50)
 
-#Arbol entrada
+#----------------------------------------------------
+#                Barra herramientas
+#----------------------------------------------------
+#
+#                 1. import/export 
+#
+#----------------------------------------------------
+grupo_import_export = Frame(frame_tools_bar)
+grupo_import_export.pack( anchor='center', side='left', padx=20)
+#----------------------------------------------------
+#                Barra herramientas
+#----------------------------------------------------
+#
+#            1.1 import/export: Botones
+#
+#----------------------------------------------------
+from funciones.seleccionar_directorios import *
+
+img_boton = tkinter.PhotoImage(file="extractText/app_sources/icons/openFolder.png")
+button_dir_in  = Button(grupo_import_export, image=img_boton,  command = lambda: abrir_directorio(arbol))
+button_dir_in.config(width=35, height=35)
+button_dir_in.pack(padx=5, side='left')
+Hovertip(button_dir_in, text="Seleccionar el directorio fuente de videos", hover_delay=500)
+
+img_boton2 = tkinter.PhotoImage(file="extractText/app_sources/icons/outPutFolder.png")
+button_dir_out  = Button(grupo_import_export, image=img_boton2, text = "Directorio Entrada")
+button_dir_out.config(width=35, height=35)
+button_dir_out.pack(padx=5, side='right')
+Hovertip(button_dir_out, text="Seleccionar el directorio de salida para los videos", hover_delay=500)
+#----------------------------------------------------
+#
+#                 2. Tools Bar 
+#
+#----------------------------------------------------
+grupo_tools_bar = Frame(frame_tools_bar)
+grupo_tools_bar.pack( anchor='center', side='left', padx=20)
+#----------------------------------------------------
+#                Barra herramientas
+#----------------------------------------------------
+#
+#            2.1 Tools Bar: eraser
+#
+#----------------------------------------------------
+img_boton3 = tkinter.PhotoImage(file="extractText/app_sources/icons/eraser.png")
+button_eraser = Button(grupo_tools_bar, image=img_boton3, text = "Directorio Entrada")
+button_eraser.config(width=35, height=35)
+button_eraser.pack(padx=5, side='left')
+Hovertip(button_eraser, text="Elimina los segmentos de texto\nque no quiere que sean reconocidos dentro del video", hover_delay=500)
+
+img_boton4 = tkinter.PhotoImage(file="extractText/app_sources/icons/SelectUser.png")
+button_selec_user = Button(grupo_tools_bar, image=img_boton4, text = "Directorio Entrada")
+button_selec_user.config(width=35, height=35)
+button_selec_user.pack(padx=5, side='left')
+Hovertip(button_selec_user, text="Selecciona el segmento de la persona que quiere reconocer", hover_delay=500)
+
+img_boton5 = tkinter.PhotoImage(file="extractText/app_sources/icons/pointer.png")
+button_pointer = Button(grupo_tools_bar, image=img_boton5, text = "Directorio Entrada")
+button_pointer.config(width=35, height=35)
+button_pointer.pack(padx=5, side='right')
+Hovertip(button_selec_user, text="Usar el puntero", hover_delay=500)
+
+
+
+#----------------------------------------------------
+#                 Arbol de Entrada
+#----------------------------------------------------
 frame_Tree_in = Frame(frame_root)
 frame_Tree_in.pack(side="left", anchor='n')
-frame_Tree_in.config(bg="lightblue", width=200, height=480)
+frame_Tree_in.config(width=200, height=480)
+
+#from funciones.seleccionar_directorios import item_selected
+arbol = CheckboxTreeview(frame_Tree_in)
+#arbol = ttk.Treeview(frame_Tree_in)
+arbol.bind("<<TreeviewSelect>>", item_selected)
+arbol.pack(anchor='n',fill='both')
+
 
 
 #Visualizador
@@ -49,11 +140,53 @@ frame_Fragmentos = Frame(frame_root)
 frame_Fragmentos.config(bg="purple")
 frame_Fragmentos.place(x=0, y = 531, width=200, height=200)
 
-
-# Herramientas de video
+#----------------------------------------------------
+#               Herramientas de video
+#----------------------------------------------------
+#
+#            1 Frame Control de Video
+#
+#----------------------------------------------------
 frame_herrsamientas_de_video = Frame(frame_root)
 frame_herrsamientas_de_video.place(x=200, y=531, width=680, height=200)
 frame_herrsamientas_de_video.config(bg="red", width= 1200, height=190)
+#----------------------------------------------------
+#               Herramientas de video
+#----------------------------------------------------
+#----------------------------------------------------
+#
+#            1.1 Botones procesar
+#
+#----------------------------------------------------
+frame_botones_procesar = Frame(frame_herrsamientas_de_video)
+frame_botones_procesar.config(bg="white")
+frame_botones_procesar.pack(fill='x', anchor='center')
+
+contenedorBotones = Frame(frame_botones_procesar)
+contenedorBotones.config(bg="white")
+contenedorBotones.pack(anchor='center', expand=True)
+
+# Boton Proceso con texto
+button_procesar_con_texto = Button(contenedorBotones, text="Procesar con texto")
+button_procesar_con_texto.pack(padx=10, side='left', anchor='center', expand=False)
+
+# Boton Proceso con texto
+button_procesar_con_voz = Button(contenedorBotones, text="Procesar con voz")
+button_procesar_con_voz.pack(padx=10, side='left', anchor='center', expand=False)
+
+#----------------------------------------------------
+#               Herramientas de video
+#----------------------------------------------------
+#----------------------------------------------------
+#
+#            1.2 Control de video
+#
+#----------------------------------------------------
+frame_control_de_video = Frame(frame_herrsamientas_de_video)
+frame_control_de_video.config(bg="pink", height=180)
+frame_control_de_video.pack(fill='x', anchor= 's')
+
+
 if __name__ == "__main__":
     
     frame_root.mainloop()
--- pruebaMovie.py
+++ pruebaMovie.py
@@ -1,258 +1,203 @@
-# @autor: Magno Efren
-# Youtube: https://www.youtube.com/c/MagnoEfren/
-# Reproductor de Musica
+import tkinter as tk
+import vlc
+from tkinter import filedialog
+from datetime import timedelta
 
-from tkinter import Button, Label,Tk,filedialog, ttk, Frame, PhotoImage
-import pygame
-import random
-import mutagen
+class MediaPlayerApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Media Player")
+        self.geometry("800x600")
+        self.configure(bg="#f0f0f0")
+        self.initialize_player()
+        
+    def initialize_player(self):
+        self.instance = vlc.Instance()
+        self.media_player = self.instance.media_player_new()
+        self.current_file = None
+        self.playing_video = False
+        self.video_paused = False
+        self.create_widgets()
 
-#from mutagen.mp3 import MP3
-#pygame.mixer.pre_init(frequency=44100)
+        
+    def create_widgets(self):
+        self.media_canvas = tk.Canvas(self, bg="black", width=800, height=400)
+        self.media_canvas.pack(pady=10, fill=tk.BOTH, expand=True)
+        self.select_file_button = tk.Button(
+            self,
+            text="Select File",
+            font=("Arial", 12, "bold"),
+            command=self.select_file,
+        )
+        self.select_file_button.pack(pady=5)
+        self.time_label = tk.Label(
+            self,
+            text="00:00:00 / 00:00:00",
+            font=("Arial", 12, "bold"),
+            fg="#555555",
+            bg="#f0f0f0",
+        )
+        self.time_label.pack(pady=5)
+        self.control_buttons_frame = tk.Frame(self, bg="#f0f0f0")
+        self.control_buttons_frame.pack(pady=5)
+        
+    def create_widgets(self):
+        self.media_canvas = tk.Canvas(self, bg="black", width=800, height=400)
+        self.media_canvas.pack(pady=10, fill=tk.BOTH, expand=True)
+        self.select_file_button = tk.Button(
+            self,
+            text="Select File",
+            font=("Arial", 12, "bold"),
+            command=self.select_file,
+        )
+        self.select_file_button.pack(pady=5)
+        self.time_label = tk.Label(
+            self,
+            text="00:00:00 / 00:00:00",
+            font=("Arial", 12, "bold"),
+            fg="#555555",
+            bg="#f0f0f0",
+        )
+        self.time_label.pack(pady=5)
+        self.control_buttons_frame = tk.Frame(self, bg="#f0f0f0")
+        self.control_buttons_frame.pack(pady=5)
+        
+        self.play_button = tk.Button(
+            self.control_buttons_frame,
+            text="Play",
+            font=("Arial", 12, "bold"),
+            bg="#4CAF50",
+            fg="white",
+            command=self.play_video,
+        )
+        self.play_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.pause_button = tk.Button(
+            self.control_buttons_frame,
+            text="Pause",
+            font=("Arial", 12, "bold"),
+            bg="#FF9800",
+            fg="white",
+            command=self.pause_video,
+        )
+        self.pause_button.pack(side=tk.LEFT, padx=10, pady=5)
+        self.stop_button = tk.Button(
+            self.control_buttons_frame,
+            text="Stop",
+            font=("Arial", 12, "bold"),
+            bg="#F44336",
+            fg="white",
+            command=self.stop,
+        )
+        self.stop_button.pack(side=tk.LEFT, pady=5)
+        self.fast_forward_button = tk.Button(
+            self.control_buttons_frame,
+            text="Fast Forward",
+            font=("Arial", 12, "bold"),
+            bg="#2196F3",
+            fg="white",
+            command=self.fast_forward,
+        )
+        self.fast_forward_button.pack(side=tk.LEFT, padx=10, pady=5)
+        self.rewind_button = tk.Button(
+            self.control_buttons_frame,
+            text="Rewind",
+            font=("Arial", 12, "bold"),
+            bg="#2196F3",
+            fg="white",
+            command=self.rewind,
+        )
+        self.rewind_button.pack(side=tk.LEFT, pady=5)
+        self.progress_bar = VideoProgressBar(
+            self, self.set_video_position, bg="#e0e0e0", highlightthickness=0
+        )
+        self.progress_bar.pack(fill=tk.X, padx=10, pady=5)
+        
+    def select_file(self):
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Media Files", "*.mp4 *.avi")]
+        )
+        if file_path:
+            self.current_file = file_path
+            self.time_label.config(text="00:00:00 / " + self.get_duration_str())
+            self.play_video()
+    
+    def get_duration_str(self):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            total_duration_str = str(timedelta(milliseconds=total_duration))[:-3]
+            return total_duration_str
+        return "00:00:00"
 
-pygame.mixer.init()
-pygame.mixer.init(frequency=44100)
-cancion_actual =''
-direcion = ''
+    def play_video(self):
+        if not self.playing_video:
+            media = self.instance.media_new(self.current_file)
+            self.media_player.set_media(media)
+            self.media_player.set_hwnd(self.media_canvas.winfo_id())
+            self.media_player.play()
+            self.playing_video = True  
+    def fast_forward(self):
+        if self.playing_video:
+            current_time = self.media_player.get_time() + 10000
+            self.media_player.set_time(current_time)
 
-def abrir_archivo():
-	global direcion, pos, n,cancion_actual
-	pos = 0
-	n = 0
-	direcion = filedialog.askopenfilenames(initialdir ='/', 
-											title='Escoger la cancion(es)', 
-										filetype=(('mp3 files', '*.mp3*'),('All files', '*.*')))
-	n = len(direcion)
-	cancion_actual = direcion[pos]
+    def rewind(self):
+        if self.playing_video:
+            current_time = self.media_player.get_time() - 10000
+            self.media_player.set_time(current_time)
+    def pause_video(self):
+        if self.playing_video:
+            if self.video_paused:
+                self.media_player.play()
+                self.video_paused = False
+                self.pause_button.config(text="Pause")
+            else:
+                self.media_player.pause()
+                self.video_paused = True
+                self.pause_button.config(text="Resume")
+    def stop(self):
+        if self.playing_video:
+            self.media_player.stop()
+            self.playing_video = False
+        self.time_label.config(text="00:00:00 / " + self.get_duration_str())
 
-	nombre_cancion = cancion_actual.split('/')
-	nombre_cancion = nombre_cancion[-1]
+    def set_video_position(self, value):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            position = int((float(value) / 100) * total_duration)
+            self.media_player.set_time(position)
 
-lista = []
-for i in range(50,200,10):
-	lista.append(i)
-
-def iniciar_reproduccion():
-	global cancion_actual, direcion, pos, n, actualizar
-	barra1['value'] = random.choice(lista)
-	barra2['value'] = random.choice(lista)
-	barra3['value'] = random.choice(lista)
-	barra4['value'] = random.choice(lista)
-	barra5['value'] = random.choice(lista)
-	barra6['value'] = random.choice(lista)
-	barra7['value'] = random.choice(lista)
-	barra8['value'] = random.choice(lista)
-	barra9['value'] = random.choice(lista)
-	barra10['value'] = random.choice(lista)
-	barra11['value'] = random.choice(lista)
-	barra12['value'] = random.choice(lista)
-	barra13['value'] = random.choice(lista)
-	barra14['value'] = random.choice(lista)
-	barra15['value'] = random.choice(lista)
-	barra16['value'] = random.choice(lista)
-	barra17['value'] = random.choice(lista)
-	barra18['value'] = random.choice(lista)
-	barra19['value'] = random.choice(lista)
-	barra20['value'] = random.choice(lista)
-
-	cancion_actual = direcion[pos]
-	nombre_cancion = cancion_actual.split('/')
-	nombre_cancion = nombre_cancion[-1]
-	nombre['text']= nombre_cancion
-
-	time = pygame.mixer.music.get_pos()
-	x = int(int(time)*0.001)
-	tiempo['value']= x  #posicion de la cancion
-
-	y = float(int(volumen.get())*0.1)
-	pygame.mixer.music.set_volume(y)
-	nivel['text']= int(y*100)
-
-	audio = mutagen.File(cancion_actual)	
-	log = audio.info.length
-	minutos, segundos = divmod(log, 60)
-
-	minutos, segundos = int(minutos), int(segundos)
-	tt = minutos*60 + segundos
-	tiempo['maximum']= tt  # tiempo total de la cancion
-	texto['text']= str(minutos) + ":" + str(segundos)
-	
-	actualizar = ventana.after(100 , iniciar_reproduccion)
-
-	if x == tt:
-		ventana.after_cancel(actualizar)
-		texto['text']= "00:00"
-		detener_efecto()
-		if pos != n:
-			pos = pos + 1
-			ventana.after(100 , iniciar_reproduccion)
-			pygame.mixer.music.play()
-		if pos == n:
-			pos = 0
-
-def iniciar():
-	global cancion_actual
-	pygame.mixer.music.load(cancion_actual)
-	pygame.mixer.music.play()
-	iniciar_reproduccion()
-
-def retroceder():
-	global pos,n
-
-	if pos >0:
-		pos = pos-1
-	else:
-		pos = 0
-	cantidad['text'] = str(pos)+'/'+str(n)
-
-def adelantar():
-	global pos, n
-
-	if pos == n-1:
-		pos = 0
-	else:
-		pos = pos + 1
-	cantidad['text'] = str(pos)+'/'+str(n)
+    def update_video_progress(self):
+        if self.playing_video:
+            total_duration = self.media_player.get_length()
+            current_time = self.media_player.get_time()
+            progress_percentage = (current_time / total_duration) * 100
+            self.progress_bar.set(progress_percentage)
+            current_time_str = str(timedelta(milliseconds=current_time))[:-3]
+            total_duration_str = str(timedelta(milliseconds=total_duration))[:-3]
+            self.time_label.config(text=f"{current_time_str}/{total_duration_str}")
+        self.after(1000, self.update_video_progress)
 
 
-def detener_efecto():
-	barra1['value'] = 50
-	barra2['value'] = 60
-	barra3['value'] = 70
-	barra4['value'] = 80
-	barra5['value'] = 90
-	barra6['value'] = 100
-	barra7['value'] = 90
-	barra8['value'] = 80
-	barra9['value'] = 70
-	barra10['value'] = 60
-	barra11['value'] = 60
-	barra12['value'] = 70
-	barra13['value'] = 80
-	barra14['value'] = 90
-	barra15['value'] = 100
-	barra16['value'] = 90
-	barra17['value'] = 80
-	barra18['value'] = 70
-	barra19['value'] = 60
-	barra20['value'] = 50
+class VideoProgressBar(tk.Scale):
+    def __init__(self, master, command, **kwargs):
+        kwargs["showvalue"] = False
+        super().__init__(
+            master,
+            from_=0,
+            to=100,
+            orient=tk.HORIZONTAL,
+            length=800,
+            command=command,
+            **kwargs,
+        )
+        self.bind("<Button-1>", self.on_click)
+    
+    def on_click(self, event):
+        if self.cget("state") == tk.NORMAL:
+            value = (event.x / self.winfo_width()) * 100
+            self.set(value)
 
-def stop():
-	global actualizar
-	pygame.mixer.music.stop()
-	ventana.after_cancel(actualizar)
-	detener_efecto()   
-def pausa():
-	global actualizar
-	pygame.mixer.music.pause()
-	ventana.after_cancel(actualizar)
-	detener_efecto()
-def continuar():
-	pygame.mixer.music.unpause()
-	ventana.after(100 , iniciar_reproduccion)
-ventana =Tk()
-ventana.title('Reproductor de Musica')
-ventana.iconbitmap('icono.ico')
-ventana.config(bg='black')
-ventana.resizable(0,0)
-
-estilo = ttk.Style()
-estilo.theme_use('clam')
-estilo.configure("Vertical.TProgressbar", foreground='green2', background='green2',troughcolor='black',
-	bordercolor='black',lightcolor='green2', darkcolor='green2')
-
-frame1 = Frame(ventana, bg='black', width=600, height=350)
-frame1.grid(column=0,row=0, sticky='nsew')
-frame2 = Frame(ventana, bg='black', width=600, height=50)
-frame2.grid(column=0,row=1, sticky='nsew')
-
-barra1 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") #,takefocus=True mode='determinate',
-barra1.grid(column=0,row=0, padx = 1)
-barra2 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra2.grid(column=1,row=0, padx = 1)
-barra3 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra3.grid(column=2,row=0, padx = 1)
-barra4 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra4.grid(column=3,row=0, padx = 1)
-barra5 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra5.grid(column=4,row=0, padx = 1)
-barra6 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra6.grid(column=5,row=0, padx = 1)
-barra7 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") #,takefocus=True
-barra7.grid(column=6,row=0, padx = 1)
-barra8 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra8.grid(column=7,row=0, padx = 1)
-barra9 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra9.grid(column=8,row=0, padx = 1)
-barra10 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra10.grid(column=9,row=0, padx = 1)
-barra11 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra11.grid(column=10,row=0, padx = 1)
-barra12 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra12.grid(column=11,row=0, padx = 1)
-barra13 = ttk.Progressbar(frame1, orient= 'vertical',length=300,  maximum=300, style="Vertical.TProgressbar")
-barra13.grid(column=12,row=0, padx = 1)
-barra14 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra14.grid(column=13,row=0, padx = 1)
-barra15 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra15.grid(column=14,row=0, padx = 1)
-barra16 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra16.grid(column=15,row=0, padx = 1)
-barra17 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra17.grid(column=16,row=0, padx = 1)
-barra18 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra18.grid(column=17,row=0, padx = 1)
-barra19 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra19.grid(column=18,row=0, padx = 1)
-barra20 = ttk.Progressbar(frame1, orient= 'vertical', length=300,  maximum=300, style="Vertical.TProgressbar") 
-barra20.grid(column=19,row=0, padx = 1)
-
-estilo1 = ttk.Style()
-estilo1.theme_use('clam')
-estilo1.configure("Horizontal.TProgressbar", foreground='red', background='black',troughcolor='DarkOrchid1',
-																bordercolor='#970BD9',lightcolor='#970BD9', darkcolor='black')
-
-tiempo = ttk.Progressbar(frame2, orient= 'horizontal', length = 390, mode='determinate',style="Horizontal.TProgressbar")
-tiempo.grid(row=0, columnspan=8, padx=5)
-texto = Label(frame2, bg='black', fg='green2', width=5)
-texto.grid(row=0,column=8)
-
-nombre = Label(frame2, bg='black', fg='red', width=55)
-nombre.grid(column=0, row=1, columnspan=8, padx=5)
-cantidad = Label(frame2, bg='black', fg='green2')
-cantidad.grid(column=8, row=1)
-
-imagen1  = PhotoImage(file ='carpeta.png')
-imagen2  = PhotoImage(file ='play.png')
-imagen3  = PhotoImage(file ='pausa.png')
-imagen4 = PhotoImage(file ='repetir.png')
-imagen5 = PhotoImage(file ='stop.png')
-imagen6 = PhotoImage(file ='anterior.png')
-imagen7 = PhotoImage(file ='adelante.png')
-
-boton1 = Button(frame2, image= imagen1, command= abrir_archivo)
-boton1.grid(column=0, row=2, pady=10)
-boton2 = Button(frame2, image= imagen2, bg='yellow', command=iniciar)
-boton2.grid(column=1, row=2, pady=10)
-boton3 = Button(frame2,image= imagen3, bg='red', command=stop)
-boton3.grid(column=2, row=2, pady=10)
-boton4 = Button(frame2,image= imagen4, bg='blue', command=pausa)
-boton4.grid(column=3, row=2, pady=10)
-boton5 = Button(frame2, image= imagen5, bg='green2',command=continuar)
-boton5.grid(column=4, row=2, pady=10)
-atras = Button(frame2, image= imagen6, bg='orange',command= retroceder)
-atras.grid(column=5, row=2, pady=10)
-adelante = Button(frame2, image= imagen7, bg='green',command=adelantar)
-adelante.grid(column=6, row=2, pady=10)
-
-volumen = ttk.Scale(frame2, to = 10, from_ =0, orient='horizontal',length=90, style= 'Horizontal.TScale')
-volumen.grid(column=7, row=2)
-
-style = ttk.Style()
-style.configure("Horizontal.TScale", bordercolor='green2', troughcolor='black', background= 'green2', 
-	foreground='green2',lightcolor='green2',darkcolor='black')  
-
-nivel = Label(frame2, bg='black', fg='green2', width=3)
-nivel.grid(column=8,row=2)
-
-ventana.mainloop()
+if __name__ == "__main__":
+    app = MediaPlayerApp()
+    app.update_video_progress()
+    app.mainloop()
